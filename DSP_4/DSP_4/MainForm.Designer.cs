64ed45a90ef7ed4574951d2a99b3a9058082746d
Fedarenchyk-16 <70834989+Fedarenchyk-16@users.noreply.github.com>
2022-10-14 21:46:37 +0300

    feat:add files

---
+++ DSP_4/DSP_4/MainForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace DSP_4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.Button();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(16, 225);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(1088, 162);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            this.chart2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(16, 395);
+            this.chart2.Margin = new System.Windows.Forms.Padding(4);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(1088, 189);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(1145, 202);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите вид сглаживания";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(8, 149);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(317, 39);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Медианная фильтрация";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton3_Checked);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(8, 36);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(324, 39);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Скользящее усреднение";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton2_Checked);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(8, 93);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(367, 39);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Парабола четвертой степени";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton1_Checked);
+            // 
+            // chart3
+            // 
+            this.chart3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Location = new System.Drawing.Point(16, 592);
+            this.chart3.Margin = new System.Windows.Forms.Padding(4);
+            this.chart3.Name = "chart3";
+            this.chart3.Size = new System.Drawing.Size(1088, 200);
+            this.chart3.TabIndex = 5;
+            this.chart3.Text = "chart3";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1501, 1025);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Алгоритмы сглаживания";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button radioButton2;
+        private System.Windows.Forms.Button radioButton1;
+        private System.Windows.Forms.Button radioButton3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+    }
+}
+
---
+++ DSP_4/DSP_4/MainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace DSP_4
+{
+    public partial class MainForm : Form
+    {
+        Series DataSer_1, DataSer_2, DataSer_3, DataSer_4, DataSer_5, DataSer_6;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Calculate(1, NoisySignal.FilteringType.Parabolic);
+        }
+
+        private void Calculate(int freq, NoisySignal.FilteringType ft)
+        {
+            NoisySignal hs = new NoisySignal(10, freq, 0, 512);
+            double[] fs=null;
+            switch (ft)
+            {
+                case NoisySignal.FilteringType.Parabolic:
+                    fs = hs.ps;
+                    break;
+                case NoisySignal.FilteringType.Median:
+                    fs = hs.ms;
+                    break;
+                case NoisySignal.FilteringType.Sliding:
+                    fs = hs.ss;
+                    break;
+                default:
+                    break;
+            }
+            chart1.Series.Clear();
+            chart1.Legends.Clear();
+            DataSer_1 = new Series();
+            Legend l1 = new Legend();
+            chart1.Legends.Add(l1);
+            l1.Title = "Сигналы";
+            DataSer_1.ChartType = SeriesChartType.Line;
+            DataSer_1.Color = Color.Purple;
+            DataSer_1.Name = "Исходный сигнал";
+            DataSer_2 = new Series();
+            DataSer_2.ChartType = SeriesChartType.Line;
+            DataSer_2.Color = Color.Green;
+            DataSer_2.Name = "Cглаженный сигнал";
+            for (int i = 0; i <= 359; i++)
+            {
+                DataSer_1.Points.AddXY(2 * Math.PI * i / 360, hs.signVal[i]);
+                DataSer_2.Points.AddXY(2 * Math.PI * i / 360, fs[i]);
+            }
+            chart1.ResetAutoValues();
+            chart1.Series.Add(DataSer_1);
+            chart1.Series.Add(DataSer_2);
+            hs.Operate(ft);
+            chart2.Series.Clear();
+            chart2.Legends.Clear();
+            chart3.Series.Clear();
+            chart3.Legends.Clear();
+            Legend l2 = new Legend();
+            chart2.Legends.Add(l2);
+            l2.Title = "Фазовый спектр";
+            Legend l3 = new Legend();
+            l3.Title = "Амплитудный спектр";
+            chart3.Legends.Add(l3);
+            DataSer_3 = new Series();
+            DataSer_3.ChartType = SeriesChartType.Candlestick;
+            DataSer_3.Color = Color.Red;
+            DataSer_3.LabelBorderWidth = 10;
+            DataSer_3.Name = "Исходный сигнал";
+            DataSer_4 = new Series();
+            DataSer_4.ChartType = SeriesChartType.Candlestick;
+            DataSer_4.Color = Color.Red;
+            DataSer_4.Name = "Исходный сигнал";
+            DataSer_5 = new Series();
+            DataSer_5.ChartType = SeriesChartType.Candlestick;
+            DataSer_5.Color = Color.Green;
+            DataSer_5.Name = "Сглаженный сигнал";
+            DataSer_6 = new Series();
+            DataSer_6.ChartType = SeriesChartType.Candlestick;
+            DataSer_6.Color = Color.Green;
+            DataSer_6.Name = "Сглаженный сигнал";
+            for (int i = 0; i <= 49; i++)
+            {
+                DataSer_3.Points.AddXY(i, hs.phaseSp[i]);
+                DataSer_4.Points.AddXY(i, hs.amplSp[i]);
+                DataSer_5.Points.AddXY(i, hs.psp[i]);
+                DataSer_6.Points.AddXY(i, hs.asp[i]);
+            }
+            chart2.ResetAutoValues();
+            chart2.Series.Add(DataSer_3);
+            chart2.Series.Add(DataSer_5);
+
+            chart3.ResetAutoValues();
+            chart3.Series.Add(DataSer_4);
+            chart3.Series.Add(DataSer_6);
+        }
+
+        private void radioButton1_Checked(object sender, EventArgs e)
+        {
+            Calculate(1, NoisySignal.FilteringType.Parabolic);
+        }
+
+        private void radioButton2_Checked(object sender, EventArgs e)
+        {
+            Calculate(1, NoisySignal.FilteringType.Median);
+        }
+
+        private void radioButton3_Checked(object sender, EventArgs e)
+        {
+            Calculate(1, NoisySignal.FilteringType.Sliding);
+        }
+    }
+}
